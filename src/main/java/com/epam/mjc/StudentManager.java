5d544e72507273505355cdd7622713d651c6dd4b
nilufar <ninimamatova0403@gmail.com>
2024-03-13 14:58:33 +0500

    Custom Exceptions

--- src/main/java/com/epam/mjc/StudentManager.java
+++ src/main/java/com/epam/mjc/StudentManager.java
@@ -1,31 +1,34 @@
 package com.epam.mjc;
 
 
+
 public class StudentManager {
 
   private static final long[] IDs = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11};
 
   public Student find(long studentID) throws Student.StudentNotFoundException {
 
-
-    throw new Student.StudentNotFoundException( "Could not find student with ID " + studentID);}
-
-
-
-
-
-
-  public static void main(String[] args) {
-    StudentManager manager = new StudentManager();
+      for (long id : IDs) {
+          if (studentID == id) {
+              return Student.getValueOf(studentID);
+          }
+      }
 
 
-try {
-  for (int i = 0; i < IDs.length; i++) {
-    Student student = manager.find(IDs[i]);
-    System.out.println("Student name " + student.getName());
+        throw new Student.StudentNotFoundException("Could not find student with ID " + studentID);
+
+
+
+
+
+
   }
-}catch ( Student.StudentNotFoundException ex){
-  System.err.print(ex);
-}
-  }
+
+
+
+
+
+
+
+
 }
